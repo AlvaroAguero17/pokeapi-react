07c747a854a1bf59b532bb4b3f5d1b6d6799b5d4
AlvaroAguero17 <alvaroawero10@gmail.com>
2023-01-02 17:04:16 -0300

    solucion 404 33

--- src/AppRouter.jsx
+++ src/AppRouter.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Route, Routes, Navigate } from "react-router-dom";
-import { Header } from "./components/Header";
+// import { Header } from "./components/Header";
 import { IndexPage } from "./pages/IndexPage";
 import { PokePage } from "./pages/PokePage";
 import { SearchPage } from "./pages/SearchPage";
