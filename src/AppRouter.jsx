ee1a918e9b3db63634a1d033a3db8fd4efd2491a
AlvaroAguero17 <alvaroawero10@gmail.com>
2023-01-02 17:22:18 -0300

    todo ok

--- src/AppRouter.jsx
+++ src/AppRouter.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Route, Routes, Navigate } from "react-router-dom";
-// import { Header } from "./components/Header";
+import { Header } from "./components/Header";
 import { IndexPage } from "./pages/IndexPage";
 import { PokePage } from "./pages/PokePage";
 import { SearchPage } from "./pages/SearchPage";
@@ -8,11 +8,11 @@ import { SearchPage } from "./pages/SearchPage";
 export const AppRouter = () => {
   return (
     <Routes>
-      {/* <Route path="/" element={<Header />}> */}
-      <Route index element={<IndexPage />} />
-      <Route path="/pokemon/:id" element={<PokePage />} />
-      <Route path="/search" element={<SearchPage />} />
-      {/* </Route> */}
+      <Route path="/" element={<Header />}>
+        <Route index element={<IndexPage />} />
+        <Route path="pokemon/:id" element={<PokePage />} />
+        <Route path="search" element={<SearchPage />} />
+      </Route>
 
       <Route path="*" element={<Navigate to="/" />} />
     </Routes>
